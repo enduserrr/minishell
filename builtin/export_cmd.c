d23695409bd60b5f0074bc8732f02977ea8bf062
eleppala <eleppala@student.hive.fi>
2024-05-28 14:32:15 +0300

    export works now with multiple args, export checks if arg is ok, if arg already exists it changes its value.

--- builtin/export_cmd.c
+++ builtin/export_cmd.c
@@ -13,14 +13,38 @@
 #include "../includes/minishell.h"
 
 /*
- * Export shows envlist like export does: 
+ * Export without commands shows envlist like export does: 
  *  - declare -x USER="eleppala"
- *  
+ * export with args adds valid args to variable list
+ *  - arg needs to start with letter
+ * 
+ * toimii nyt usealla argumentilla kerralla
+ * muokkaa jo olemassa olevan argumentin arvoa
+ * tarkistaa etta argumentti on kelpo 
+ * 
  * Puutteet: 
  *  - export lista aakkosjarjestykseen
- *  - jonkunlainen check, jos variable on jo olemassa 
  *  - uuden variablen lisaaminen aakkosjarjestettyyn listaan
  */
+
+int     key_exists(char **arr, t_tools *tools)
+{
+    t_env   *temp; 
+
+    temp = tools->env_list;
+    while(temp != NULL)
+    {
+        if (ft_strncmp(temp->key, arr[0], ft_strlen(arr[0])) == 0)
+        {
+            free(temp->value);
+            temp->value = NULL;
+            temp->value = arr[1];
+            return 0;
+        } 
+        temp = temp->next;
+    }
+    return (-1); 
+}
 
 void    add_new(t_tools *tools, char *str)
 {
@@ -28,7 +52,12 @@ void    add_new(t_tools *tools, char *str)
     t_env   *new;
     char    **temp_arr;
 
+    new = NULL;
     temp_arr = ft_split(str, '=');
+    if (!temp_arr)
+        perror("malloc");
+    if (key_exists(temp_arr, tools) == 0)
+        return ;
     new = malloc(sizeof(t_env));
     new->key = ft_strdup(temp_arr[0]);
     new->value = ft_strdup(temp_arr[1]);
@@ -39,11 +68,6 @@ void    add_new(t_tools *tools, char *str)
         temp = temp->next;
     temp->next = new;
 }
-
-/*
- * outputs envlist like export does, 
- *  - later make them in alphabetical order, good for now
- */
 
 void    output_export(t_env *env)
 {
@@ -57,10 +81,33 @@ void    output_export(t_env *env)
     }
 }
 
+int     validate_arg(char *str)
+{
+    int     i;
+
+    i = 1;
+    if (ft_isalpha(str[0]) == 0)
+    {
+        printf("export: `%s': is not a valid identifier\n", str);
+        return (-1);
+    }
+    return (0);
+}
+
 void	export_cmd(t_tools *tools)
 {
+    int i;
+    
+    i = 1;
     if (tools->split_rl[1] == NULL)
         output_export(tools->env_list);
     if (tools->split_rl[1])
-        add_new(tools, tools->split_rl[1]);
+    {
+        while (tools->split_rl[i] != NULL)
+        {
+            if (validate_arg(tools->split_rl[i]) == 0)
+                add_new(tools, tools->split_rl[i]);
+            i ++;
+        }
+    }
 }
